b70c08c7b5b3f465660de7ca635bad07de8cf289
Ziopod <ziopod@gmail.com>
2020-07-28 21:05:44 +0200

    Mise au couleurs de la saison

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: env.mainTitle },
-      { hid: 'og:site_name', property: 'og:site_name', content: env.mainDesc },
+      { hid: 'description', name: 'description', content: env.mainDesc },
+      { hid: 'og:site_name', property: 'og:site_name', content: env.mainTitle },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og.description', name: 'og.description', content: env.mainDesc },
       { hid: 'og:image', property: 'og:image', content: env.url + '/apple-touch-icon.png' },
