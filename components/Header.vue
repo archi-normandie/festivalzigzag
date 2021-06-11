92f360c96f0d6c3a35fc75402ec91431df7fda5c
Ziopod <ziopod@gmail.com>
2021-06-11 19:11:03 +0200

    Ajout d'une présentation de menu plus standardisé

--- components/Header.vue
+++ components/Header.vue
@@ -1,33 +1,33 @@
 <template>
   <header>
     <div class="wrapper">
-      <h1 class="title-site">
-        <nuxt-link to="/">
-          <span class="title-site--img" />
-          <span class="title-site--txt">Zigzag</span>
-        </nuxt-link>
-        <p class="title-site--slogan">
-          Le long de la Vallée de la Seine
-        </p>
-      </h1>
       <nav class="block menu">
-        <ul>
-          <li>
+        <h1 class="title-site menu-section-left">
+          <nuxt-link to="/">
+            <span class="title-site--img" />
+            <span class="title-site--txt">Zigzag</span>
+          </nuxt-link>
+          <p class="title-site--slogan">
+            Le long de la Vallée de la Seine
+          </p>
+        </h1>
+        <ul class="is-quiet menu-section-right">
+          <li class="menu-item">
             <nuxt-link to="/lieux">
               Lieux
             </nuxt-link>
           </li>
-          <li>
+          <li class="menu-item">
             <nuxt-link to="/agenda">
               Agenda
             </nuxt-link>
           </li>
-          <li>
+          <li class="menu-item">
             <nuxt-link to="/journal">
               Journal
             </nuxt-link>
           </li>
-          <li>
+          <li class="menu-item">
             <nuxt-link to="/pages/a-propos">
               À propos
             </nuxt-link>
