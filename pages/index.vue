3505056a37dc916c84f879e1839276b7497debd2
Ziopod <ziopod@gmail.com>
2022-06-29 20:33:11 +0200

    Tentavie de rendu de l'effet produit

--- pages/index.vue
+++ pages/index.vue
@@ -42,8 +42,17 @@ export default {
     // height: 60vh;
     width: 53rem;
   }
-  .filter-multiply {
-    mix-blend-mode: multiply;
-  }
+
+  // Solution possible : utiliser plusieurs svg de fond. un avec un filtre
+  // multiply, l'autre en premier plan sans filtre. Conserver les même
+  // dimensions de viewort pour faciliter le calage.
+  // mix-blend-mode: multiply;
+
+  // Ne fonctionne que sur les SVG incorporée.
+  // .filter-multiply {
+  //   fill: blue;
+    // mix-blend-mode: multiply;
+    // opacity: 0.6;
+  // }
 }
 </style>
