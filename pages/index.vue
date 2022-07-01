78bc7473e58a3f1365898cb39e08d0e305ee902f
Ziopod <ziopod@gmail.com>
2022-07-01 15:33:58 +0200

    Ajustement du pied de page

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,28 @@
       <span class="sr">
         Zigzag — Festival d'architecture et des arts de l'espace, du 1er au 16 octobre 2022
       </span>
+      <div class="home-summary level">
+        <div class="left">
+          <h1>
+            1<sup>er</sup> › 16 octobre 2022
+          </h1>
+          <p class="sub-header">
+            Le long de la vallée de la seine
+          </p>
+        </div>
+        <div class="right level">
+          <ul class="left">
+            <li>Déambulations</li>
+            <li>Installations</li>
+            <li>Parcours</li>
+          </ul>
+          <ul class="right">
+            <li>Randonnées</li>
+            <li>Tablées</li>
+            <li>Visites</li>
+          </ul>
+        </div>
+      </div>
     </nuxt-link>
   </main>
 </template>
@@ -55,5 +77,48 @@ export default {
     // mix-blend-mode: multiply;
     // opacity: 0.6;
   // }
+}
+
+.level {
+  display: inline-flex;
+  justify-content: space-between;
+  margin-left: 0;
+  .left {
+    justify-content: flex-start;
+    text-align: left;
+  }
+  .right {
+    justify-content: flex-end;
+    text-align: right;
+  }
+}
+
+.home-summary {
+  position: absolute;
+  right: $line-height;
+  bottom: calc($line-height * 3);
+  padding: $paddings-large;
+  background: $brand-color-secondary;
+  color: $brand-color-secondary-invert;
+  text-transform: uppercase;
+
+  h1 {
+    color: inherit;
+    font-size: $fs24;
+    margin-top: 0;
+    margin-bottom: .2rem;;
+  }
+  .sub-header { font-size: $fs18; }
+  .level {
+    font-size: $fs14;
+    ul {
+      list-style: none;
+      li:note(:last-child) {
+        margin-bottom: calc($line-height / 3);
+        padding-bottom: calc($line-height / 3);
+        border-bottom: 1px solid;
+      }
+    }
+  }
 }
 </style>
